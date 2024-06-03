766a0eb189e7b60175138ba044896c97d0b2c3b8
AndrewLucier <113055343+AndrewLucier@users.noreply.github.com>
2024-06-02 21:52:22 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+function setPageInfo(){
+    // add my name to the h1
+    document.getElementById("h1PageTitle").textContent = "Andrew Lucier's Bio Info";
+    // add my major to the major div
+    document.getElementById("divMajor").textContent = "Major: Computer Science";
+    // add my email to the email div
+    document.getElementById("divEmail").textContent = "Email: andlucie@uat.edu";
+    // add my graduation to the grad date div
+    document.getElementById("divGradDate").textContent = "Expected Graduation Date: Summer 2025";
+
+    //console.log("Success!");
+}
